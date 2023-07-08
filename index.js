bae7f0bddd0fbebd7c3924221f92636b146620b0
Ulises Mariano Melgarejo <73831325+Nemo3003@users.noreply.github.com>
2023-07-08 11:28:28 -0300

    Update index.js

--- index.js
+++ index.js
@@ -22,18 +22,11 @@ app.use(bodyParser.json());
 
 app.use(morgan('dev'));
 
-const corsOptions = {
-  origin: 'https://heartfelt-twilight-23e637.netlify.app',
-  credentials: true,
-};
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', corsOptions.origin);
-  res.header('Access-Control-Allow-Credentials', 'true');
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-
-app.use(cors(corsOptions));
 
 const port_nd = 8081;
 app.listen(port_nd, () => console.log(`Listening on port ${port_nd}`));
